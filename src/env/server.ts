c83aa7242a99ba35602b191d4f633d6998e9930f
sashkode <alexd@oust.email>
2025-10-21 19:27:09 -0400

    feat(env): create client and server env files

---
+++ src/env/server.ts
@@ -0,0 +1,13 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { vercel } from '@t3-oss/env-nextjs/presets-zod';
+
+/**
+ * Server-side environment configuration
+ * These variables are only available on the server
+ */
+export const env = createEnv({
+  extends: [vercel()],
+  server: {
+  },
+  experimental__runtimeEnv: process.env,
+});
