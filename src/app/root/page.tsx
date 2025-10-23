11b4cdbbcbe6a229686a36092290ea898d041482
sashkode <alexd@oust.email>
2025-10-22 21:18:28 -0400

    style(home): limit height to device viewport

--- src/app/root/page.tsx
+++ src/app/root/page.tsx
@@ -5,8 +5,8 @@ import { SubdomainLink } from '~/lib/navigation/client/components/subdomain-link
 
 export default async function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between px-16 py-32 sm:items-start">
+    <div className="flex min-h-dvh items-center justify-center font-sans">
+      <main className="flex min-h-dvh w-full max-w-3xl flex-col items-center justify-between px-16 py-32 sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority={true} />
         <WavyBackground className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs font-semibold text-3xl text-black leading-10 tracking-tight dark:text-zinc-50">Home</h1>
@@ -18,7 +18,7 @@ export default async function Home() {
             or a random{' '}
             <SubdomainLink subdomain={`/${Math.random().toString(36).substring(2, 15)}`} className="font-medium text-zinc-950 dark:text-zinc-50">
               Subdomain
-            </SubdomainLink>{' '}
+            </SubdomainLink>
             .
           </p>
         </WavyBackground>
