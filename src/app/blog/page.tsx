96e3a67a994d0d6bf3ccc65da47ab9e939421fba
sashkode <alexd@oust.email>
2025-11-24 00:19:15 -0500

    feat(safe-page): migrate remaining pages to use safe-page building

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 
 import { SubdomainLink } from '~/lib/navigation/client/components/subdomain-link';
+import { Page } from '~/lib/navigation/server/next-safe-page';
 
-export default async function Blog() {
+export default Page.create({
+  path: '/blog',
+  name: 'blog',
+}).page(() => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
@@ -20,4 +24,4 @@ export default async function Blog() {
       </main>
     </div>
   );
-}
+});
