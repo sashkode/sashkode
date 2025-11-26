8d89854155f342543fb688913006b43f767f0bfd
sashkode <alexd@oust.email>
2025-11-26 00:35:32 -0500

    chore(safe-page): cleanup demo stuff

--- src/app/root/page.tsx
+++ src/app/root/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 
-import { HelloButton } from '~/lib/home/client/components/hello-button';
 import { WavyBackground } from '~/lib/home/client/components/wavy-background';
 import { SubdomainLink } from '~/lib/navigation/client/components/subdomain-link';
 import { Page } from '~/lib/navigation/server/next-safe-page';
@@ -13,7 +12,6 @@ export default Page.create({
     <div className="flex min-h-dvh items-center justify-center font-sans">
       <main className="flex min-h-dvh w-full max-w-3xl flex-col items-center justify-between px-16 py-32 sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority={true} />
-        <HelloButton />
         <WavyBackground className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs font-semibold text-3xl text-black leading-10 tracking-tight dark:text-zinc-50">Home</h1>
           <p className="max-w-md text-lg text-zinc-600 leading-8 dark:text-zinc-400">
